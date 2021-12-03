e1fe9e6c7dd1df39f7d9941c765ea5cc4e38be3f
murariugabriela <murariugabriela01@gmail.com>
2021-12-03 07:48:00 +0200

    Added files and directories according to json

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import sys
 import json
+import os
 
 
 def parse_json(json_object: dict, path: str):
     files = []
     for k, v in json_object.items():
         if isinstance(v, dict):
+            try:
+                os.mkdir(path + "\\" + k)
+            except FileExistsError:
+                print(f"Error: file {path}\\{k} already exists")
             files.append([k, parse_json(v, path + "\\" + k)])
         else:
-            print(path, k, v)
+            f = open(path + "\\" + k, "w")
+            f.write(v)
+            f.close()
             files.append([k, v])
     return files
 
