01538861d7804e4d8b15a6653b14767d97d57905
murariugabriela <murariugabriela01@gmail.com>
2021-12-03 10:21:17 +0200

    Added type checks for parameters from cmdline

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def parse_json(json_object: dict, path: str):
             try:
                 os.mkdir(path + "\\" + k)
             except FileExistsError:
-                print(f"Error: file {path}\\{k} already exists")
+                print(f"Error: Directory {path}\\{k} already exists\n")
             files.append([k, parse_json(v, path + "\\" + k)])
         else:
             f = open(path + "\\" + k, "w")
@@ -21,6 +21,16 @@ def parse_json(json_object: dict, path: str):
 
 
 if __name__ == '__main__':
-    print(sys.argv[1], sys.argv[2], sep="\n")
-    json_obj = json.loads(sys.argv[2])
-    parse_json(json_obj, sys.argv[1])
+    print(sys.argv[1], sys.argv[2], "\n", sep="\n")
+    if os.path.isfile(sys.argv[1]):
+        print("Error: given directory is not a directory\n")
+    else:
+        if not os.path.isdir(sys.argv[1]):
+            print("Error: given directory does exist... Currently creating it...\n")
+            os.mkdir(sys.argv[1])
+        try:
+            # for a json format of " {'dir': {'file': 'contains'}, 'file': 'contains'}"
+            json_obj = json.loads(sys.argv[2].replace("'", "\""))
+            parse_json(json_obj, sys.argv[1])
+        except json.JSONDecodeError:
+            print("Error: given parameter does not respect json format")
