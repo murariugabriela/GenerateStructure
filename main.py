495c354fa5f0bc4aa8d401efeb5c5ca1d65224f2
murariugabriela <murariugabriela01@gmail.com>
2021-12-13 09:48:42 +0200

    Added permission checks for files Changed path concatenation to os.path.join changed mkdir function to makedirs for exist check

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ import json
 import os
 
 
+def check_file_writable(file):
+    if os.path.exists(file):
+        if os.path.isfile(file):
+            return os.access(file, os.W_OK)
+        else:
+            return False
+    # target does not exist, check perms on parent dir
+    parent_dir = os.path.dirname(file)
+    if not parent_dir:
+        parent_dir = '.'
+    # target is creatable if parent dir is writable
+    return os.access(parent_dir, os.W_OK)
+
+
 def parse_json(json_object: dict, path: str):
     files = []
     for k, v in json_object.items():
         if isinstance(v, dict):
             try:
-                os.mkdir(path + "\\" + k)
+                os.makedirs(os.path.join(path, k), exist_ok=True)
             except FileExistsError:
-                print(f"Error: Directory {path}\\{k} already exists\n")
-            files.append([k, parse_json(v, path + "\\" + k)])
+                print(f"Error: Directory {os.path.join(path, k)} already exists\n")
+            files.append([k, parse_json(v, os.path.join(path, k))])
         else:
-            f = open(path + "\\" + k, "w")
-            f.write(v)
-            f.close()
+            if check_file_writable(os.path.join(path, k)):
+                with open(os.path.join(path, k), "w") as file:
+                    file.write(v)
+            else:
+                print("Error: We don't have write access for this file")
             files.append([k, v])
     return files
 
@@ -26,11 +42,16 @@ if __name__ == '__main__':
         print("Error: given directory is not a directory\n")
     else:
         if not os.path.isdir(sys.argv[1]):
-            print("Error: given directory does exist... Currently creating it...\n")
+            print("Error: given directory does not exist... Currently creating it...\n")
             os.mkdir(sys.argv[1])
         try:
             # for a json format of " {'dir': {'file': 'contains'}, 'file': 'contains'}"
-            json_obj = json.loads(sys.argv[2].replace("'", "\""))
-            parse_json(json_obj, sys.argv[1])
+            # fisier json ca param
+            if os.access(sys.argv[2], os.R_OK):
+                with open(sys.argv[2], "r") as f:
+                    json_obj = json.loads(f.read())
+                    parse_json(json_obj, sys.argv[1])
+            else:
+                print("Error: We don't have read access for this file")
         except json.JSONDecodeError:
             print("Error: given parameter does not respect json format")
